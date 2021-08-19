a7a9ad58fe8a8698d90ce851de1222299626580f
fathurzoy <fathurrahmanf123@gmail.com>
2021-08-19 17:26:15 +0700

    pemisahan tempat redux

--- src/container/pages/Product/CardProduct/Counter.jsx
+++ src/container/pages/Product/CardProduct/Counter.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux"; //hi order componnet
+import ActionType from "../../../../redux/reducer/globalActiontype";
 
 export class Counter extends Component {
   // state = {
@@ -70,8 +71,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handlePlus: () => dispatch({ type: "PLUS_ORDER" }),
-    handleMinus: () => dispatch({ type: "MINUS_ORDER" }),
+    handlePlus: () => dispatch({ type: ActionType.PLUS_ORDER }),
+    handleMinus: () => dispatch({ type: ActionType.MINUS_ORDER }),
   };
 };
 
